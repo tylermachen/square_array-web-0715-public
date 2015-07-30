d0bf65f7f98ec7f212ea430e3373284e09fc9ddc
Tyler Machen <tylermachen@gmail.com>
2015-07-30 16:34:57 -0400

    create square array method

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
-  # your code here
+  result = []
+  array.each { |elem| result << elem ** 2 }
+  result
 end
+
+# this also works
+=begin
+def square_array(array)
+  array.map { |elem| elem ** 2 }
+end
+=end
